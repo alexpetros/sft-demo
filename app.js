dc9845b1271ac1386ee615b13ff845688e4ddab1
Alexander Petros <apetros15@gmail.com>
2022-01-21 00:18:03 -0500

    Add SFT URL logic

--- app.js
+++ app.js
@@ -4,17 +4,37 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var images = require('./routes/users');
 
 var app = express();
 
 app.use(logger('dev'));
+app.set('view engine', 'ejs')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const defaultSft = "https://media.gettyimages.com/photos/disabled-woman-training-taekwondo-with-her-coach-picture-id1350650910?s=2048x2048"
+let currentSft = defaultSft
 
+/* GET home page. */
+const router = express.Router();
+router.get('/', (req, res) => { res.redirect('/home') })
+
+router.get('/home', (req, res) => {
+  res.render('index', { sfturl: currentSft });
+});
+
+/* Sft */
+router.post('/sft', (req, res, next) => {
+  console.log(req.body)
+  if (req.body) {
+    currentSft = req.body.sfturl
+
+  }
+});
+
+
+app.use('/', router);
 module.exports = app;
